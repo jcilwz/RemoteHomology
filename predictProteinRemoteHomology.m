5b18a7f088121ac4c933900a3b6b0ceb2a65e412
jcilwz <63843752@qq.com>
2019-01-23 19:25:10 +0800

    2019-1-23 19:25

--- predictProteinRemoteHomology.m
+++ predictProteinRemoteHomology.m
@@ -1,15 +1,13 @@
 
 function [family, prob] = predictProteinRemoteHomology(queryProteinFile)
     load 3187sequences_Grey21PSSM.mat %psepssm
-    load 3187FunctionDomainMatch_0.1.mat
+    load 3187pfams.mat
     load 3187sequencesFamily.mat
-
-    x=[0.3 0.02 0.28 0.4];
+    load pfamA-Clans.mat
     
     [queryheads,~]= fastaread(queryProteinFile);
-    [trainheads,~] = fastaread('7329seqs.fasta');
-    
-    
+    [trainheads,~] = fastaread('3187seqs.fasta');
+        
     tlen = length(trainheads);
     
     if iscell(queryheads)
@@ -32,52 +30,31 @@ function [family, prob] = predictProteinRemoteHomology(queryProteinFile)
     family = cell(1,qlen);
     prob = cell(1,qlen);
     
-    % psi-blast bit score
-    cmd = ['psiblast -db 7329seqsdb -query ' queryProteinFile ' -out blastpairfile -outfmt 6'];
-    system(cmd, '-echo')
-    [bitscore,~] = pairVals('blastpairfile', queryheads, trainheads);
-    
-    % hmmer score
-    cmd = ['jackhmmer  --tblout hmmerscoreout.txt ' queryProteinFile ' 7329seqs.fasta'];
-    system(cmd, '-echo');
-    hmmscore = ones(qlen,tlen);
-    fid = fopen('hmmerscoreout.txt','r');
-    tline = fgetl(fid);
-    while (ischar(tline) && ~isempty(tline))
-        if( startsWith(tline, '#'))
-            tline = fgetl(fid);
-            continue;
-        else
-            A = split(tline);
-            targetProtein = A(1);
-            queryProtein = A(3);
-            i = findStringInCell(queryheads, queryProtein);
-            j = findStringInCell(trainheads, targetProtein);
-            hmmscore(i,j) = str2double(A(6));
-            tline = fgetl(fid);
-        end
-    end
-    fclose(fid);
-    
-    % get pssm of query proteins
+    % 1. grey-PSSM
+    % 1.1 get pssm of query proteins
     tp=blastpssm(queryProteinFile,'swissprot');
     if iscell(tp)
         p = tp;
     else
         p{1} = tp;
     end
-    % glcm
-    queryGlcm = getPSSM_GLCM(p);
-    % grey(2,1) PSSM
+
+    % 1.2 grey(2,1) PSSM
     queryPsepssm=greyPsePssm(p,2);
-    
+    % 2. functional domain set index
+    dist_DSI = zeros(qlen,tlen);
+    queryFams = buildFunctionDomainVect(queryProteinFile);
     for i = 1 : qlen
-        r = GreyIncidenceDegree(queryPsepssm(i,:),psepssm);
-        d = GreyIncidenceDegree(queryGlcm(i,:), X);
-        b = mapminmax(bitscore(i,:),0,1);
-        h = mapminmax(hmmscore(i,:),0,1);
-        dis=x(1)*r + x(2)*b + x(3)*d + x(4)*h;
-        [B,I] = sort(dis,'descend');
+        for j = 1 : tlen
+            dist_DSI = pfamcmp(queryFams{i},pfams{j},clanDict);
+        end
+    end
+    
+    % 3. merge distance of grey incidence alansys and domain set index
+    for i = 1 : qlen
+        dist_GIA = GreyIncidenceDegree(queryPsepssm(i,:),psepssm);
+        dist = dist_GIA + dist_DSI(i,:)
+        [B,I] = sort(dist,'descend');
         family{i} = familyId(I(1));
         prob{i} = B(I(1));
     end
