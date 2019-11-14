7156ccfdc0b116e1d45f67ba93f3de11bce6931a
javafalcon <javafalcon@163.com>
2019-11-14 09:52:42 +0800

    2019-11-14

--- program/prepareWork.py
+++ program/prepareWork.py
@@ -11,15 +11,38 @@ Created on Wed Nov 13 09:59:18 2019
 """
 
 from util import PfamClan
+import scipy.io as sio
 clanDict = {}
 
-with open(r'e:\360Downloads\Pfam-A.clans.tsv','r',encoding='utf-8') as fo:
+with open('Pfam-A.clans.tsv','r',encoding='utf-8') as fo:
     for line in fo:
         line = line.replace("\n","")
         s = line.split("\t")
         pc = PfamClan(s[1],s[2],s[3],s[4])
         clanDict[s[0]] = pc
 
-import scipy.io as sio
-sio.savemat('pfamClan.mat', clanDict)
 
+from util import pfamcmp
+testdata_pfams=sio.loadmat('independent_pfams.mat')
+traindata_pfams=sio.loadmat('train_pfams.mat')
+pfam_dist={}
+testkeys = list(testdata_pfams.keys())
+trainkeys = list(traindata_pfams.keys())
+totalTest = len(testkeys[3:])
+totalTrain = len(trainkeys[3:])
+count = 1
+for k1 in testkeys[3:]:
+    print("\n        {}/{}==>{:.2f}%".format(count,totalTest,count/totalTest * 100),end="")
+    dist=[]
+    pf1=testdata_pfams[k1]
+    
+    n = 1
+    for k2 in trainkeys[3:]:
+        print("\r{:.2f}%".format(n/totalTrain * 100),end="")
+        pf2=traindata_pfams[k2]
+        d=pfamcmp(pf1,pf2,clanDict)
+        dist.append(d)
+        n = n + 1
+    pfam_dist[k1] = dist
+    count = count + 1
+        
