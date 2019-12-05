9a058b527abddcc42d5b96b3d6abe87233ed6b39
javafalcon <javafalcon@163.com>
2019-12-05 16:31:18 +0800

    test prediction

--- program/performanceTest.py
+++ program/performanceTest.py
@@ -8,9 +8,9 @@ Created on Fri Nov 29 09:53:21 2019
 
 import numpy as np
 from util import PfamClan,pfamcmp
-"""
-calculate pfam distance
-"""
+import scipy.io as sci
+from SeqFormulate import greyPseAAC
+#calculate pfam caln
 clanDict = {}
 with open('Pfam-A.clans.tsv','r',encoding='utf-8') as fo:
     for line in fo:
@@ -19,86 +19,48 @@ with open('Pfam-A.clans.tsv','r',encoding='utf-8') as fo:
         pc = PfamClan(s[1],s[2],s[3],s[4])
         clanDict[s[0]] = pc
         
-import scipy.io as sci
-pfmat = sci.loadmat('7329pfams_np.mat')
-pfs=pfmat['pfs']
-pfam_dist = np.zeros((7329,7329))
 
-for i in range(7329):
-    for j in range(7329):
-        pfam_dist[i,j] = pfamcmp(pfs[0,i][0].split(','),pfs[0,j][0].split(','),clanDict)  
+pfm_dist= sci.loadmat('pfam_dist.mat')
 
-"""
-calculate grey incidence degree
-"""
-with open('trainPseAAC_7329.npy','rb') as f:
-    data = np.load(f)
-"""
-from greymodel import GreyIncidenceDegree
-gia_dist = np.zeros((7329,7329))
+#calculate distance
+from Bio import SeqIO
+test_records = list(SeqIO.parse('scope_independent.fa','fasta'))
+num_test = len(test_records)
+codeTypes = ['MolecularWeight','Hydrophobicity','PK1','PK2','PI']
+test_pseAAC = []
+for seq_record in test_records:
+    test_pseAAC.append(greyPseAAC(str(seq_record.seq), codeTypes))
+test_pseAAC = np.array(test_pseAAC)
 
-for j in range(20,30,2):
-    data[:,j] = data[:,j] * 50
-    data[:,j+1] = data[:,j+1] * 0.1   
-for i in range(7329):
-    gia_dist[i,:] = GreyIncidenceDegree(data[i,:],data)
-np.savez('7329_pfam_weightedgrePseAAC_dist',pfam=pfam_dist, gia=gia_dist)
+train_pseAAC = np.load('trainPseAAC.npy')
+num_train = 145187
+eucl_dist = np.zeros((num_test, num_train))
 
+print("calculating euclidean distance between test samples and train samples......")
+for i  in range(num_test):
+    print("\r{}/{}==>{:.2f%}".format(i, num_test, i/num_test))
+    for j in range(num_train):
+        eucl_dist[i,j] = np.linalg.norm(test_pseAAC[i] - train_pseAAC[j])
+        
+with open('scope_test_train_euclidean_distance.npy','wb') as f:
+    np.save(f, eucl_dist)
+    
 # jack knife test   
 w = [0.5, 0.5]
 count = 0
-familymat = sci.loadmat('7329sequencesFamily.mat')
-fms = familymat['familyId']       
-for i in range(7329):
-    d = pfam_dist[i] * w[0] + gia_dist[i] * w[1]
+familymat = sci.loadmat('train_family.mat')
+
+print("calculating prediction accuracy......")  
+for i in range(num_test):
+    d = (1-pfm_dist[i]) * w[0] + eucl_dist[i] * w[1]
     indx_arr = np.argsort(d)
-    
-    if fms[0,indx_arr[-2]][0] == fms[0,i][0]:
+    seq = test_records[i]
+    desc = seq.descriptioin
+    faml = (desc.split(" "))[1]
+
+    if familymat[str(indx_arr[1])][0] == faml:
         count += 1
+    print("\r{}/{}==>{:.2f%}".format(i, num_test, i/num_test))
 print("predicted result: in {0} samples corrected predict {1}: {2:.2%}".format(7329,count,count/7329))    
-"""
 
-"""
-# eculidean distance
-eucl_dist = np.zeros((7329,7329))
-for i in range(7329):
-    print("\r Calculating Eucl dist {:.2f}%".format(i/7329 *100), end="")
-    for j in range(7329):
-        eucl_dist[i,j] = np.linalg.norm(data[i] - data[j])
-np.savez('7329_weightedgrePseAAC_eucldist',pfam=pfam_dist, eucl=eucl_dist)
 
-# jack knife test
-w = [0.7, 0.3]
-count = 0
-familymat = sci.loadmat('7329sequencesFamily.mat')
-fms = familymat['familyId']       
-for i in range(7329):
-    print("\r Testing {:.2f}%".format(i/7329 *100), end="")
-    d = (1-pfam_dist[i]) * w[0] + eucl_dist[i] * w[1]
-    indx_arr = np.argsort(d)
-    
-    if fms[0,indx_arr[1]][0] == fms[0,i][0]:
-        count += 1
-print("predicted result: in {0} samples corrected predict {1}: {2:.2%}".format(7329,count,count/7329))    
-"""
-
-# cosin distance
-cos_dist = np.zeros((7329,7329))
-for i in range(7329):
-    print("\rCalculating Cosin dist {:.2f}%".format(i/7329 * 100), end="")
-    for j in range(7329):
-        cos_dist[i,j] = np.dot(data[i],data[j])/(np.linalg.norm(data[i])*(np.linalg.norm(data[j])))
-np.save('7329_cosindist',cosdist=cos_dist)
-# jack knife test
-w = [1, 1]
-count = 0
-familymat = sci.loadmat('7329sequencesFamily.mat')
-fms = familymat['familyId']       
-for i in range(7329):
-    print("\r Testing {:.2f}%".format(i/7329 *100), end="")
-    d = pfam_dist[i] * w[0] + cos_dist[i] * w[1]
-    indx_arr = np.argsort(d)
-    
-    if fms[0,indx_arr[-2]][0] == fms[0,i][0]:
-        count += 1
-print("predicted result: in {0} samples corrected predict {1}: {2:.2%}".format(7329,count,count/7329)) 
