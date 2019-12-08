8728f0f9c9648166747f88cb792e19a47910afd7
jcilwz <63843752@qq.com>
2019-12-08 10:27:03 +0800

    2019-12-8

--- program/performanceTest_3187.py
+++ program/performanceTest_3187.py
@@ -10,6 +10,7 @@ import numpy as np
 from util import PfamClan,pfamcmp
 import scipy.io as sci
 from SeqFormulate import greyPseAAC
+from greymodel import GreyIncidenceDegree
 # load trains family
 mat_family = sci.loadmat(r'E:\360yunpan\MyProgram\RemoteHomo\3187sequencesFamily.mat')
 familyId = mat_family['familyId']
@@ -24,26 +25,29 @@ train_records = list(SeqIO.parse('3187seqs.fasta','fasta'))
 num_train = len(train_records)
 codeTypes = ['MolecularWeight','Hydrophobicity','PK1','PK2','PI']
 train_pseAAC = []
-for seq_record in train_records:
-    train_pseAAC.append(greyPseAAC(str(seq_record.seq), codeTypes))
-train_pseAAC = np.array(train_pseAAC)
-
-eucl_dist = np.zeros((num_train, num_train))
-
 weight = np.ones((30,))
 weight[20:30:2] = 40
 weight[21:30:2] = 0.1
-print("calculating euclidean distance between test samples and train samples......")
+for seq_record in train_records:
+    v = greyPseAAC(str(seq_record.seq), codeTypes) * weight
+    train_pseAAC.append(v)
+train_pseAAC = np.array(train_pseAAC)
+
+#eucl_dist = np.zeros((num_train, num_train))
+gid_dist = np.zeros((num_train, num_train))
+
+print("calculating grey incidence degree distance between train samples......")
 for i  in range(num_train):
     print("\r{}/{}==>{:.2%}".format(i, num_train, i/num_train), end="")
-    for j in range(num_train):
-        eucl_dist[i,j] = np.linalg.norm(train_pseAAC[i] * weight- train_pseAAC[j] * weight)
+    gid_dist[i] = GreyIncidenceDegree(train_pseAAC[i], train_pseAAC)
+    #for j in range(num_train):
+        #eucl_dist[i,j] = np.linalg.norm(train_pseAAC[i] * weight- train_pseAAC[j] * weight)
         
-with open('3187_euclidean_weight_distance.npy','wb') as f:
-    np.save(f, eucl_dist)
+#with open('3187_euclidean_weight_distance.npy','wb') as f:
+#   np.save(f, eucl_dist)
     
 # jack knife test   
-w = [1, 5]
+w = [1, 10]
 count = 0
 print("calculating prediction accuracy......")  
 for i in range(num_train):
@@ -52,10 +56,11 @@ for i in range(num_train):
     head = desc.split(" ")
     sid = head[0]
     faml = head[1]
-    d = (1-pfam_dist[sid][0]) * w[0] + eucl_dist[i] * w[1]
+    #d = (1-pfam_dist[i]) * w[0] + eucl_dist[i] * w[1]
+    d = pfam_dist[i] * w[0] + gid_dist[i] * w[1]
     indx_arr = np.argsort(d)   
 
-    if familyId[0,indx_arr[1]] == faml:
+    if familyId[0,indx_arr[-2]] == faml:
         count += 1
     print("\r{}/{}==>{:.2%}".format(i, num_train, i/num_train), end="")
 print("predicted result: in {0} samples corrected predict {1}: {2:.2%}".format(num_train,count,count/num_train))    
